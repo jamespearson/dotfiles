76ce47b695470c1c0ba5702739afaba02486a4b1
James Pearson <james@needcode.co.uk>
2020-01-07 13:46:26 +0000

    🎉 initial App store installations

---
+++ appstore.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+brew install mas
+
+read -p "Have you signed in to the App Store?"
+
+mas install 1333542190  # 1Password 7 (7.4.1)
+mas install 1462114288  # Grammarly for Safari (9.10)
+mas install 1482454543  # Twitter (8.4.2)
+mas install 979047768   # Browserism (2.3.1)
+mas install 1292198261  # iMazing HEIC Converter (1.0.10)
+mas install 918858936   # Airmail (3.6.73)
+mas install 1191449274  # ToothFairy (2.6.1)
+mas install 803453959   # Slack (4.2.0)
+mas install 1477385213  # Save to Pocket (1.1)
+mas install 1147396723  # WhatsApp (0.3.9309)
+mas install 1351639930  # Gifski (2.5.0)
+mas install 747648890   # Telegram (5.8)
+mas install 937984704   # Amphetamine (4.2)
+mas install 413857545   # Divvy (1.5.2)
+mas install 409203825   # Numbers (6.2.1)
+mas install 567740330   # JSON Editor (1.31)
