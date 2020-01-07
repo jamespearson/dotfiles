83aabbca98942f7be0d5ea088d6c370e9f24aa85
James Pearson <james@needcode.co.uk>
2020-01-07 13:24:01 +0000

    ➕ Added git-open

--- brew.sh
+++ brew.sh
@@ -122,6 +122,8 @@ ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 
+npm install --global git-open
+
 #curl -sSL https://get.rvm.io | bash
 
 npm install -g react-native-cli
